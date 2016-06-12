a9c05024c4a7939537f6e74ecc2699501f68ccd5
Podolski, Henri <henrip@gmail.com>
2016-06-12 10:33:24 +0200

    Refactor form canvas example for cross browser use

--- src/examples/canvas-form-data/js/components/vote-form.js
+++ src/examples/canvas-form-data/js/components/vote-form.js
@@ -12,10 +12,18 @@ export default class VoteForm extends mix(Component).with(ItemSelectorToMembers)
     }
 
     onVoteButtonClick(evt) {
-        // console.log(`${this}.onVoteButtonClick(${evt})` );
+        // console.log(`${this}.onVoteButtonClick(${evt})`, this.el.language, this.el.language.value);
 
-        if (this.el.language && this.el.language.value) {
-            this.trigger('vote', {selected: this.el.language.value});
+        let selectedLanguage;
+
+        for (let i = 0; i < this.el.language.length; i++) {
+            if (this.el.language[i].checked === true) {
+                selectedLanguage = this.el.language[i].value;
+            }
+        }
+
+        if (selectedLanguage) {
+            this.trigger('vote', {selected: selectedLanguage});
             Array.from(this.el.language).forEach(radio => radio.checked = false);
         }
     }
